0820e953c52a1634b28a97a5f0359f4efeaca8fa
Eric J <ejuquel@yahoo.fr>
2021-05-15 16:19:38 +0200

    add houses page

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,5 +1,5 @@
 import { useRef, useState } from "react";
-import Link from "next/link";
+// import Link from "next/link";
 import { Image } from "cloudinary-react";
 import ReactMapGL, { Marker, Popup, ViewState } from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
@@ -16,37 +16,56 @@ export default function Map({}: Iprops) {
     longitude: 49.88728,
     zoom: 10,
   });
+  const [mapStyle, setMapStyle] = useState("mapbox/satellite-v9");
 
   return (
     <>
       <div className="w-full h-12 mx-auto  flex items-center justify-evenly">
-        <div className="flex items-center mr-4 mb-4">
+        <div className="flex items-center m-auto">
           <input
             id="radio1"
             type="radio"
             name="radio"
             className="hidden"
-            checked
+            value="mapbox/satellite-v9"
+            checked={mapStyle === "mapbox/satellite-v9"}
+            onChange={(e) => setMapStyle(e.target.value)}
           />
           <label htmlFor="radio1" className="flex items-center cursor-pointer">
             <span className="w-4 h-4 inline-block mr-1 rounded-full border border-grey"></span>
-            First choice
+            Satellite
           </label>
         </div>
 
-        <div className="flex items-center mr-4 mb-4">
-          <input id="radio2" type="radio" name="radio" className="hidden" />
+        <div className="flex items-center m-auto">
+          <input
+            id="radio2"
+            type="radio"
+            name="radio"
+            className="hidden"
+            value="mapbox/streets-v11"
+            checked={mapStyle === "mapbox/streets-v11"}
+            onChange={(e) => setMapStyle(e.target.value)}
+          />
           <label htmlFor="radio2" className="flex items-center cursor-pointer">
             <span className="w-4 h-4 inline-block mr-1 rounded-full border border-grey"></span>
-            Second choice
+            Streets
           </label>
         </div>
 
-        <div className="flex items-center mr-4 mb-4">
-          <input id="radio3" type="radio" name="radio" className="hidden" />
+        <div className="flex items-center m-auto">
+          <input
+            id="radio3"
+            type="radio"
+            name="radio"
+            className="hidden"
+            value="leighhalliday/ckhjaksxg0x2v19s1ovps41ef"
+            checked={mapStyle === "leighhalliday/ckhjaksxg0x2v19s1ovps41ef"}
+            onChange={(e) => setMapStyle(e.target.value)}
+          />
           <label htmlFor="radio3" className="flex items-center cursor-pointer">
             <span className="w-4 h-4 inline-block mr-1 rounded-full border border-grey"></span>
-            Third choice
+            Dark
           </label>
         </div>
       </div>
@@ -60,7 +79,7 @@ export default function Map({}: Iprops) {
           ref={(instance) => (mapRef.current = instance)}
           minZoom={5}
           maxZoom={20}
-          mapStyle="mapbox://styles/mapbox/satellite-v9"
+          mapStyle={`mapbox://styles/${mapStyle}`}
         ></ReactMapGL>
       </div>
     </>
