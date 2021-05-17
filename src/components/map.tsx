ab8f8f74adc1de732adc56c06845651e871b8245
Eric J <ejuquel@yahoo.fr>
2021-05-17 18:18:07 +0200

    issue on map.tsx

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,8 +1,7 @@
 import { useRef, useState } from "react";
 import Link from "next/link";
 import { Image } from "cloudinary-react";
-import ReactMapGL, { Marker, Popup } from "react-map-gl";
-import { ViewState } from "react-map-gl/index";
+import ReactMapGL, { Marker, Popup, ViewState } from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import { useLocalState } from "src/utils/useLocalState";
 import { HousesQuery_houses } from "src/generated/HousesQuery";
